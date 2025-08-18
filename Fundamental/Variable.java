041db72dd97bd2b8e9cb45b556239c8c626438ef
Chetan Naik <chetannaik11145@gmail.com>
2025-08-18 20:24:49 +0530

    first java code

---
+++ Fundamental/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        String c = "Chetan";
+        System.out.println(c);
+    }
+}

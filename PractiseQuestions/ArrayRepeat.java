dd12353957723c33f20df859ddb108396ca62503
Chetan Naik <chetannaik11145@gmail.com>
2025-09-02 16:51:19 +0530

    new practise set

---
+++ PractiseQuestions/ArrayRepeat.java
@@ -0,0 +1,20 @@
+package PractiseQuestions;
+
+public class ArrayRepeat {
+
+    public static boolean checkDublicate(int nums[]) {
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 2, 3};
+        System.out.println(checkDublicate(nums));
+    }
+}

d11479397fea84637dd7879d1822539df58e7869
Chetan Naik <chetannaik11145@gmail.com>
2025-09-12 20:05:58 +0530

    StringBuilders

---
+++ Strings/Anagrams.java
@@ -0,0 +1,32 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class Anagrams {
+    public static void main(String[] args) {
+        String str1 = "earth";
+        String str2 = "heart";
+
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if (str1.length() == str2.length()) {
+            char[] str1CharArray = str1.toCharArray();
+            char[] str2CharArray = str2.toCharArray();
+
+            Arrays.sort(str1CharArray);
+            Arrays.sort(str2CharArray);
+
+            boolean res = Arrays.equals(str1CharArray, str2CharArray);
+
+            if (res) {
+                System.out.println(str1 + " and " + str2 + " are anagrams of eachother.");
+            } else {
+                System.out.println(str1 + " and " + str2 + " are not anagrams ofeach other.");
+            }
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams ofeach other.");
+        }
+
+    }
+}

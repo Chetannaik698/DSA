c612352524f20f3d26be082b2e996be686c2ff8b
Chetan Naik <chetannaik11145@gmail.com>
2025-10-03 22:25:02 +0530

    recursion-progress

---
+++ Recursion/SumOfNnaturalNo.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class SumOfNnaturalNo {
+
+    public static int Sum(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n + Sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Sum(n));
+    }
+}
